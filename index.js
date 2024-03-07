b840c4c09034ed10f4cd2dd11c37f64afd986e3a
Korac Enes <korac.enes2@gmail.com>
2024-03-07 12:48:45 +0100

    variables

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
-document.getElementById("myH1").textContent = "Hello";
+// document.getElementById("myH1").textContent = "Hello";
+// document.getElementById("myP").textContent = "I like pizza";
 
-document.getElementById("myP").textContent = "I like pizza";
+// variable = A container that stores a value.
+//                   Behaves as if it were the value it contains.
+
+let age = 25;
+let price = 10.99;
+let gpa = 2.1;
+
+console.log(typeof age);
+
+console.log(`you are ${age} years old`);
+console.log(`the price i s ${price}`);
+console.log(`Your gpa is ${gpa}`);
+
+let online = true;
+console.log(`bro is online: ${online}`);
