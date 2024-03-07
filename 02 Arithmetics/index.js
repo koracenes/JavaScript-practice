7f70a0dc550c223c30ab6447f204c7330c2772e5
Korac Enes <korac.enes2@gmail.com>
2024-03-07 12:56:18 +0100

    new folders

---
+++ 02 Arithmetics/index.js
@@ -0,0 +1,3 @@
+// arithmetic operators = operands (values, variables, etc.)
+//                                         operators (+ - * /)
+//                                         ex. 11 = x + 5;
