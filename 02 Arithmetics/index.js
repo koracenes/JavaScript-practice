6bcaf12f07513a5fbf001145f75e43f5c291b7a9
Korac Enes <korac.enes2@gmail.com>
2024-03-07 13:00:18 +0100

    arithmetics

--- 02 Arithmetics/index.js
+++ 02 Arithmetics/index.js
@@ -1,3 +1,13 @@
 // arithmetic operators = operands (values, variables, etc.)
 //                                         operators (+ - * /)
 //                                         ex. 11 = x + 5;
+
+let students = 30;
+
+students = students + 1;
+students = students - 1;
+students = students * 1;
+students = students / 1;
+students = students ** 2; //**  exponent
+
+console.log(students);
