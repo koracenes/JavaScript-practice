f7bc484579ec5e11fe6cf7f8defe07f7c91e68a5
Korac Enes <korac.enes2@gmail.com>
2024-03-09 12:57:10 +0100

    user inputs

---
+++ 03 User Input/index.js
@@ -0,0 +1,11 @@
+// How to accept user input
+
+// 1. EASY WAY = window prompt
+// 2. PROFESSIONAL WAY = HTML textbox
+
+let username;
+
+document.getElementById("mySubmit").onclick = function () {
+  username = document.getElementById("myText").value;
+  document.getElementById("myH1").textContent = `Hello ${username}`;
+};
