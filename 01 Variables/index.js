531fd80604788c9c60846f2ec755f8fac100256f
Korac Enes <korac.enes2@gmail.com>
2024-03-12 11:27:37 +0100

    re arranged folders

--- 01 Variables/index.js
+++ 01 Variables/index.js
@@ -1,3 +1,7 @@
+/*====================================
+       01 VARIABLES
+======================================*/
+
 // document.getElementById("myH1").textContent = "Hello";
 // document.getElementById("myP").textContent = "I like pizza";
 
@@ -24,3 +28,55 @@ let iStudent = false;
 document.getElementById("p1").textContent = `my name is ${fullName}`;
 document.getElementById("p2").textContent = age;
 document.getElementById("p3").textContent = iStudent;
+
+/*====================================
+       02 ARITHMETICS
+======================================*/
+
+// arithmetic operators = operands (values, variables, etc.)
+//                                         operators (+ - * /)
+//                                         ex. 11 = x + 5;
+
+let students = 30;
+
+//students = students + 1;
+//students = students - 1;
+//students = students * 2;
+//students = students / 2;
+//students = students ** 2;
+//let extraStudents = students % 3;
+
+console.log(students);
+
+//students += 1;
+//students -= 1;
+//students *= 2;
+//students /= 2;
+//students **= 2;
+//students %= 2;
+
+//students++;
+//students--;
+
+/*
+    operator precedence
+    1. parenthesis ()
+    2. exponents
+    3. multiplication & division & modulo
+    4. addition & subtraction
+*/
+
+/*====================================
+        03 USER INPUT
+======================================*/
+// How to accept user input
+
+// 1. EASY WAY = window prompt
+// 2. PROFESSIONAL WAY = HTML textbox
+
+let username;
+
+document.getElementById("mySubmit").onclick = function () {
+  username = document.getElementById("myText").value;
+  document.getElementById("myH1").textContent = `Hello ${username}`;
+};
